4fbcac71b6e21fb1959d9cfced64b2b29597318d
Marc Catrisse <marc@catrisse.net>
2018-11-08 19:27:32 +0100

    refactor token ask, now checked in bot middleware

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const racoAuth = require('./racoAuth');
 const api = require('./ApiClient');
 const ip = require('ip');
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+
 bot.use((ctx, next) => {
     const start = new Date();
     console.log("persona: " + ctx.message.from.id);
     if(ctx.message.from.id !== 316789902) return null;//just for testing
     //check has valid token
-    return next(ctx).then(() => {
-        const ms = new Date() - start;
-        console.log('Response time %sms', ms);
-    })
+    private_token(ctx.message.from.id).then(function(token){
+        bot.context.token = token;
+        return next(ctx).then(() => {
+            const ms = new Date() - start;
+            console.log('Response time %sms', ms);
+        })
+    }, function(err){
+        console.log("no tengo el token, asking...");
+        ask_token();
+        return null;
+    });
 });
+
 bot.start(ctx => {
     tokenModel.find({id: ctx.message.from.id},function(err,docs){
         console.log("Find: " + docs);
@@ -26,22 +36,19 @@ bot.start(ctx => {
 });
 
 bot.hears('/data', (ctx) => {
-    private_token(ctx.message.from.id).then(function(token){
-        api.getData(token).then(function(res){
-            console.log(res);
-            ctx.reply("Nom: " + res.nom + " " + res.cognoms + "\nEmail: " + res.email);
-        }, function(err){ //error al pillar el token
-            ask_token(ctx);
-        });
+    console.log(ctx.token);
+    api.getData(ctx.token).then(function(res){
+        console.log(res);
+        ctx.reply("Nom: " + res.nom + " " + res.cognoms + "\nEmail: " + res.email);
+    }, function(err){ //error al pillar el token
+        ask_token(ctx);
     });
 });
 
 bot.hears('/foto', (ctx) => {
-    private_token(ctx.message.from.id,function(token){
-        api.getFoto(token).then(function(res){
-            ctx.replyWithPhoto({
-                source: Buffer.from(res)
-            });
+    api.getFoto(ctx.token).then(function(res){
+        ctx.replyWithPhoto({
+            source: Buffer.from(res)
         });
     });
 });
@@ -69,4 +76,5 @@ const private_token = new Promise (function (id, resolve, reject){
 function ask_token(ctx){
     ctx.reply("Autoriza: http://"+ip.address() + ":3000/auth?id="+ctx.message.from.id);
 }
+
 bot.startPolling();
