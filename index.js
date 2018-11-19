459154bf92372fef0e1829c31b3e1f98221e5cd1
Marc Catrisse <marc@catrisse.net>
2018-11-19 20:28:50 +0100

    bug token no refresh solucionado

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const api = require('./controllers/ApiController');
 const ip = require('ip');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-
+bot.context.token = null;
 bot.use((ctx, next) => {
     const start = new Date();
     console.log("persona: " + ctx.message.from.id);
-    if(ctx.message.from.id !== 316789902) return null;//just for testing todo delete this in prod
+   // if(ctx.message.from.id !== 316789902) return null;//just for testing todo delete this in prod
     //check has valid token
     private_token(ctx.message.from.id).then(function(token){
-        bot.context.token = token;
+        ctx.token = token;
         return next(ctx).then(() => {
             const ms = new Date() - start;
             console.log('Response time %sms', ms);
@@ -67,13 +67,18 @@ const private_token = function(id) {
                             client_secret: process.env.CLIENT_SECRET
                         });
                         console.log(accessToken);
-                        docs[0].updateOne({token: accessToken});
-                    }
+                        let token = docs[0];
+                        token.token = accessToken.token;
+                        token.save(function (err, item) {
+                            if (err) throw err;
+                            resolve(item.token.access_token);
+                        });
+                    }else resolve(accessToken.token.access_token);
                 }
                 catch (error) {
                     console.log('Error refreshing access token: ', error.message);
+                    reject();
                 }
-                resolve(accessToken.token.access_token);
             } else reject();
         });
     });
